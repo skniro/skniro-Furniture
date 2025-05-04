261f0c253eb0cea7d39b089e12eb833373528ffe
skniro <46392259+skniro@users.noreply.github.com>
2025-05-04 23:10:37 +0800

    Finished lamp.

--- Fabric/src/main/java/com/skniro/skniro_furniture/block/init/LampBlock.java
+++ Fabric/src/main/java/com/skniro/skniro_furniture/block/init/LampBlock.java
@@ -4,6 +4,7 @@ import com.mojang.serialization.MapCodec;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.server.world.ServerWorld;
@@ -18,6 +19,8 @@ import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.random.Random;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 import net.minecraft.world.block.WireOrientation;
 import org.jetbrains.annotations.Nullable;
@@ -25,6 +28,8 @@ import org.jetbrains.annotations.Nullable;
 public class LampBlock extends Block {
     public static final MapCodec<LampBlock> CODEC = createCodec(LampBlock::new);
     public static final BooleanProperty LIT;
+    private static final VoxelShape SHAPE = Block.createCuboidShape(0.0, 14.5, 0.0, 16.0, 16.0, 16.0);
+
 
     public MapCodec<LampBlock> getCodec() {
         return CODEC;
@@ -35,23 +40,22 @@ public class LampBlock extends Block {
         this.setDefaultState((BlockState)this.getDefaultState().with(LIT, false));
     }
 
+    @Override
     @Nullable
     public BlockState getPlacementState(ItemPlacementContext ctx) {
         return (BlockState)this.getDefaultState().with(LIT, ctx.getWorld().isReceivingRedstonePower(ctx.getBlockPos()));
     }
 
-    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit) {
         if (!world.isClient) {
-            boolean bl = state.get(LIT);
-            if (bl) {
-                world.scheduleBlockTick(pos, this, 4);
-            } else {
-                world.setBlockState(pos, (BlockState)state.cycle(LIT), 2);
-            }
+            boolean lit = state.get(LIT);
+            world.setBlockState(pos, state.with(LIT, !lit), 2);
         }
         return ActionResult.SUCCESS;
     }
 
+    @Override
     protected void scheduledTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
         if ((Boolean)state.get(LIT) && !world.isReceivingRedstonePower(pos)) {
             world.setBlockState(pos, (BlockState)state.cycle(LIT), 2);
@@ -59,8 +63,14 @@ public class LampBlock extends Block {
 
     }
 
+    @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
         builder.add(new Property[]{LIT});
+    }
+
+    @Override
+    protected VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return SHAPE;
     }
 
     static {
