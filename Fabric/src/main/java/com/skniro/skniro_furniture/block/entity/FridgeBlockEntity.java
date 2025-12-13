7d1369f8d471e2dfdd8f595e30491416abdb958a
skniro <46392259+skniro@users.noreply.github.com>
2025-12-13 23:29:07 +0800

    Add new content.

--- Fabric/src/main/java/com/skniro/skniro_furniture/block/entity/FridgeBlockEntity.java
+++ Fabric/src/main/java/com/skniro/skniro_furniture/block/entity/FridgeBlockEntity.java
@@ -1,7 +1,9 @@
 package com.skniro.skniro_furniture.block.entity;
 
+import com.skniro.skniro_furniture.block.init.FridgeBlock;
 import com.skniro.skniro_furniture.init.FurnitureStrings;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.enums.DoubleBlockHalf;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.screen.GenericContainerScreenHandler;
 import net.minecraft.screen.ScreenHandler;
@@ -16,9 +18,12 @@ public class FridgeBlockEntity extends AbstractFurnitureContainerBlockEntity {
 
     @Override
     protected Text getContainerName() {
-        return Text.translatable(FurnitureStrings.Fridge);
-    }
 
+        if(getCachedState().get(FridgeBlock.HALF) == DoubleBlockHalf.UPPER) {
+            return Text.translatable(FurnitureStrings.Fridge_UPPER);
+        }
+        return Text.translatable(FurnitureStrings.Fridge_LOWER);
+    }
     @Override
     protected ScreenHandler createScreenHandler(int syncId, PlayerInventory playerInventory) {
         return GenericContainerScreenHandler.createGeneric9x6(syncId, playerInventory, this);
--- Forge/src/main/java/com/skniro/skniro_furniture/block/entity/FridgeBlockEntity.java
+++ Forge/src/main/java/com/skniro/skniro_furniture/block/entity/FridgeBlockEntity.java
@@ -1,5 +1,6 @@
 package com.skniro.skniro_furniture.block.entity;
 
+import com.skniro.skniro_furniture.block.init.FridgeBlock;
 import com.skniro.skniro_furniture.init.FurnitureStrings;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
@@ -7,6 +8,7 @@ import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.ChestMenu;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.DoubleBlockHalf;
 
 public class FridgeBlockEntity extends AbstractFurnitureContainerBlockEntity {
 
@@ -16,7 +18,10 @@ public class FridgeBlockEntity extends AbstractFurnitureContainerBlockEntity {
 
     @Override
     protected Component getDefaultName() {
-        return Component.translatable(FurnitureStrings.Fridge);
+        if(getBlockState().getValue(FridgeBlock.HALF) == DoubleBlockHalf.UPPER) {
+            return Component.translatable(FurnitureStrings.Fridge_UPPER);
+        }
+        return Component.translatable(FurnitureStrings.Fridge_LOWER);
     }
 
     @Override
